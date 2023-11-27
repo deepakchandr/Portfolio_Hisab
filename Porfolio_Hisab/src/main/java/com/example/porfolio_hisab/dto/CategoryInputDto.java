e3b83000c361abe74a26bfeeaf9e06a18cf9d7ee
deepakchandr <116279383+deepakchandr@users.noreply.github.com>
2023-11-28 00:45:02 +0530

    Delete Porfolio_Hisab directory

--- Porfolio_Hisab/src/main/java/com/example/porfolio_hisab/dto/CategoryInputDto.java
+++
@@ -1,5 +0,0 @@
-package com.example.porfolio_hisab.dto;
-
-public class CategoryInputDto {
-
-}
--- Porfolio_Hisab/src/main/java/com/example/porfolio_hisab/service/ICategoryService.java
+++
@@ -1,7 +0,0 @@
-package com.example.porfolio_hisab.service;
-
-import com.example.porfolio_hisab.entity.Category;
-
-public interface ICategoryService {
-	public String addCategory(Category cat);
-}

fc3daa84c5d5ffc8f64928a3007fc5e7dd3f29ee
deepakchandr <116279383+deepakchandr@users.noreply.github.com>
2024-07-14 20:57:57 +0530

    Delete src directory

--- src/main/java/com/example/porfolio_hisab/dto/CategoryInputDto.java
+++
@@ -1,8 +0,0 @@
-package com.example.porfolio_hisab.dto;
-
-import lombok.Data;
-
-@Data
-public class CategoryInputDto {
-	private String categoryName;
-}
--- src/main/java/com/example/porfolio_hisab/dto/InputCategoryDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.porfolio_hisab.dto;
-
-import lombok.Data;
-
-
-@Data
-public class InputCategoryDto {
-	private String categoryName;
-}
